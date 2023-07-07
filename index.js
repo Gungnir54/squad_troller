1f7d6e1dca381aa4f7ba73589703e6feb4af7e0a
Gungnir <vincent.becker@epitech.eu>
2023-07-07 11:57:51 +0200

    push before deployement test

--- index.js
+++ index.js
@@ -10,15 +10,30 @@ bot.on("ready", async () => {
   bot.user.setActivity("Troller les gens");
 });
 
+const arrayResponse = [
+  "PTDR T ki dans l'e-sport ?",
+  "J't'emmerde je fais ce que je veux.",
+  "*/add blokedex.",
+  "Ratio plus palu.",
+  "Casse toi ca devient génant.",
+  "T ki ?",
+  "Tu t'appel pas Kevin par hasard ?",
+  "T'es qui pour me tag en fait ?",
+  "Des fois je me sens bête, ensuite je te regarde et ca va mieux.",
+  "J'aime le son de ton clavier quand tu la ferme.",
+  "Ca joue sur un 60Hz et ca se dit gamer.",
+];
+const random = arrayResponse[Math.floor(Math.random() * arrayResponse.length)];
+
 bot.on("messageCreate", async (message) => {
   if (
     message.content.toLowerCase().includes("quoicoubeh") ||
     message.content.toLowerCase().includes("t'as les crampté") ||
     message.content.toLowerCase().includes("apagnan")
   ) {
-    message.delete();
+    message.delete();    
   } else if (message.content.toLowerCase().includes("quoi")) {
-    return message.reply("Feur ?");
+    return message.reply("Feur");
   }
   if (
     message.content.toLowerCase().includes("hein")
@@ -26,6 +41,6 @@ bot.on("messageCreate", async (message) => {
     return message.reply("Deux");
   if (message.mentions.users.size > 0) {
     if (message.mentions.users.has(bot.user.id))
-      return message.reply("J't'emmerde je fais ce que je veux.");
+      return message.reply(random);
   }
 });
